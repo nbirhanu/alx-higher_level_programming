8e68c791623bae1f5483690c2f0f2133803d691e
nbirhanu <nbirhanu80gmail.com>
2022-06-01 14:38:09 +0000

    print words first_3 last_2 and middle

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("word first 3: {}".format(word_first_3))
-print("word last 2: {}".format(word_last_2))
+print("word 3 first: {}".format(word_first_3))
+print("word 2 last: {}".format(word_last_2))
 print("middle word: {}".format(middle_word))
