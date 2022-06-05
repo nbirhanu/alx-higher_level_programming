8932182086b40cafd188fc5571717637b652cb54
nbirhanu <nbirhanu80gmail.com>
2022-06-05 18:08:24 +0000

    prints the numbers from 1 to 100 separated by a space.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,4 @@ def fizzbuzz():
         if i % 5 == 0:
             print("Buzz", end="")
         else:
-            print("i", end="")
-        print(" ", end="")
+            print("{} ".format(number), end="")
