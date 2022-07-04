afb40d792d353017eec38b1db473d8ddd297a84d
nbirhabu <nbirhanu80@gmail.com>
2022-07-05 00:50:35 +0300

    cool

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry."""
+
+
+class BaseGeometry:
+    """class BaseGeometry (based on 5-base_geometry.py)."""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
