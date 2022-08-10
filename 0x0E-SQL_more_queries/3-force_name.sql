4bb18993bdb2a222e6268d23bc4a0000708faa79
nbirhabu <nbirhanu80@gmail.com>
2022-08-10 21:43:35 +0300

    3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+-- a script that creates the table force_name on your MySQL server and if it exists script shouldn't fail
+-- the database name will be passed as an argument of the mysql command
+
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
+	);
+  
